da52e173cb9cebf41fd48d6d7d508152633505d7
aitordsgn <aitordsgn@gmail.com>
2024-08-09 14:33:36 +0200

    Chore: Remove unused prettier plugins and update eslint dependencies

--- prettierrc.js
+++ prettierrc.js
@@ -1,19 +1,27 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  semi: false, // This is how you turn off semicolons, by the way
-  trailingComma: "all",
-  singleQuote: true,
-  arrowParens: "always",
-  jsxSingleQuote: false,
-  printWidth: 120,
+  semi: false, // Desactiva los puntos y comas automáticos
+  trailingComma: 'all', // Añade comas al final de objetos, arrays, etc.
+  singleQuote: true, // Usa comillas simples en lugar de comillas dobles
+  arrowParens: 'always', // Siempre coloca paréntesis en las funciones flecha
+  jsxSingleQuote: false, // Usa comillas dobles en JSX
+  printWidth: 120, // Longitud máxima de línea
   plugins: [
-    require.resolve("prettier-plugin-astro", "prettier-plugin-tailwindcss"),
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss'),
   ],
-  overrides: [{ files: "*.astro", options: { parser: "astro" } }],
   overrides: [
     {
-      files: ["*.json", "*.md", "*.toml", "*.yml"],
-      options: { useTabs: false },
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+    {
+      files: ['*.json', '*.md', '*.toml', '*.yml'],
+      options: {
+        useTabs: false,
+      },
     },
   ],
-};
+}
