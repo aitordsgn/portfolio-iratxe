6664d96e623633db4dcedd4c54a21da11cca399b
aitordsgn <aitordsgn@gmail.com>
2024-08-12 15:41:36 +0200

    Refactor: Update JSON-LD schema for better SEO and branding

--- src/js/utils.js
+++ src/js/utils.js
@@ -53,3 +53,28 @@ export function formatBlogPosts(
   }
   return filteredPosts;
 }
+
+export const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Iratxe Conde Turon",
+  "jobTitle": "Publicista y Diseñadora Gráfica",
+  "url": "http://localhost:4321/",
+  "sameAs": [
+    "https://www.linkedin.com/in/iratxeconde/",
+    "https://www.instagram.com/iratxe_design/",
+    "https://x.com/iratxeconde07/"
+  ],
+  "image": "http://localhost:4321/Portafolio.webp",
+  "description": "Publicista y diseñadora gráfica con experiencia en multimedia y branding. Conoce mis proyectos y habilidades creativas.",
+  "worksFor": {
+    "@type": "Organization",
+    "name": "Freelance"
+  },
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "Bilbao",
+    "addressRegion": "Bizkaia",
+    "addressCountry": "España"
+  }
+};
