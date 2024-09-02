cbf7c8129b7809321931e999c1ef1c3f4e75fe7d
aitordsgn <aitordsgn@gmail.com>
2024-09-02 16:30:45 +0200

    feat: Update website URLs to use www subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
+
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: 'https://www.iratxeconde.es',
-  build: {
-    inlineStylesheets: "always", // Incluye estilos CSS en línea
-  },
-  compressHTML: true, // Comprime HTML para mejor rendimiento
-  devToolbar: {
-    enabled: false, // Desactiva la barra de herramientas de desarrollo
-  },
-  
+  site: 'https://www.iratxeconde.es', 
 });
