dbf3df17d15dc5313acbfca480dc69d704771e8d
aitordsgn <aitordsgn@gmail.com>
2024-09-02 13:08:44 +0200

    refactor: Remove @astrojs/vercel from package.json and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless"; // Adaptador para despliegue en Vercel
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,17 +13,5 @@ export default defineConfig({
   devToolbar: {
     enabled: false, // Desactiva la barra de herramientas de desarrollo
   },
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true, // Habilita la analítica web para Vercel
-    },
-  }),
-  vite: {
-    build: {
-      cssMinify: "lightningcss", // Usar lightningcss para minificación de CSS
-    },
-    ssr: {
-      noExternal: ["path-to-regexp"], // Evita la externalización de módulos específicos
-    },
-  },
+  
 });
