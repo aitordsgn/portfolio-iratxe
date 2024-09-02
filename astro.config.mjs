d29fa58898d094026768765fd1ffcdba15505bc4
aitordsgn <aitordsgn@gmail.com>
2024-09-02 13:01:29 +0200

    feat: Update npm dependencies and devDependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless"; // Adaptador para despliegue en Vercel
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  site: 'https://www.iratxeconde.es',
+  build: {
+    inlineStylesheets: "always", // Incluye estilos CSS en línea
+  },
+  compressHTML: true, // Comprime HTML para mejor rendimiento
+  devToolbar: {
+    enabled: false, // Desactiva la barra de herramientas de desarrollo
+  },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // Habilita la analítica web para Vercel
+    },
+  }),
+  vite: {
+    build: {
+      cssMinify: "lightningcss", // Usar lightningcss para minificación de CSS
+    },
+    ssr: {
+      noExternal: ["path-to-regexp"], // Evita la externalización de módulos específicos
+    },
+  },
 });
